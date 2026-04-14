266eee8b5112419cf321c773ff39bb75a4dcf0de
hassnainabbas5678 <hassnainlilani@gmail.com>
2026-04-14 21:12:50 +0500

    Fixed MongoDB Atlas connection and added root route

--- server/src/server.js
+++ server/src/server.js
@@ -37,6 +37,10 @@ const PORT = process.env.PORT || 5000;
 connectDB(process.env.MONGO_URI).then(async () => {
   await ensureAdmin();  
 
+  app.get("/", (req, res) => {
+  res.send("Backend is running successfully!");
+});
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
